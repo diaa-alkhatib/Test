d4ca1032c674b7e340bafaeed686c9fc72bc2ac0
Diaa Alkhatib <alkhatib.diaaa@gmail.com>
2020-11-09 12:28:27 +0100

    Initial Commit

---
+++ src/main/java/com/diaa/Test/TestController.java
@@ -0,0 +1,11 @@
+package com.diaa.Test;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    public String get (){
+        return "hi Diaa";
+    }
+}
